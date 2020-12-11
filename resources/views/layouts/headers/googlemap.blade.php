329cfea5c16af14ac5b6c25c6af0d18352bdb1b8
HJ Tempelaar <hjtempelaar@gmail.com>
2020-12-11 14:40:38 +0100

    maar dan beter

--- resources/views/layouts/headers/googlemap.blade.php
+++ resources/views/layouts/headers/googlemap.blade.php
@@ -1,16 +1,14 @@
-<div class="header bg-gradient-respons py-7 py-lg-6">
+<div class="header py-7 py-lg-6" style="background-color: #f7b941">
     <div class="container">
-        <div class="header-body text-center">
+        <div class="header-body text-center" style="background-color: #f7b941">
             <div class="row justify-content-center">
+                <div class="col-lg-4 col-md-4">
+                  <img alt="Respons" src="https://respons.nl/wp-content/uploads/2019/06/Respons-2018_witte-gloed_NL_resized.png">
+                </div>
                 <div class="col-lg-8 col-md-8">
-                    <h1 class="text-blue">{{ __('Welkom bij de Interactieve Festival Monitor 2020.') }}</h1>
+                    <h1 class="text-black-50">{{ __('Welkom bij de Interactieve Festival Monitor 2020.') }}</h1>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="separator separator-bottom separator-skew zindex-100">
-        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
-            <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
-        </svg>
     </div>
 </div>
