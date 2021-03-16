47e661d935b70049f23b84478a4ae00bf53acc7e
HJ Tempelaar <hjtempelaar@gmail.com>
2021-03-16 16:32:21 +0100

    update

--- resources/views/googlemap.blade.php
+++ resources/views/googlemap.blade.php
@@ -14,7 +14,7 @@
             <div class="col-xl-3" >
 
                 <div style="margin-left: 20px;">
-                <div class="row text-center" style="background-color: #f7b941">
+                <div class="row col" style="background-color: #f7b941">
                     <h1 class="text-center">Respons Festival Monitor</h1>
                 </div>
                 <div class="row">
